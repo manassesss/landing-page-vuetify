ee53cb1028a2cf0b1621505f462962dd70c9acf8
manassesss <manasses@ufpi.edu.br>
2024-03-20 20:05:19 -0300

    initialized project and home section

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-app>
+    <v-main>
+      <home />
+    </v-main>
+
+    <!-- <AppFooter /> -->
+  </v-app>
+</template>
+
+<script setup>
+  import home from "./components/HomeSection.vue";
+</script>
+
+
+<style lang="scss">
+  @import url('https://fonts.googleapis.com/css2?family=Inter:wght@100..900&display=swap');
+
+  .v-application{
+    background-color: #f8f6f0;
+    font-family: "Inter", sans-serif;
+  }
+
+</style>>
---
+++ src/components/HomeSection.vue
@@ -0,0 +1,37 @@
+<template>
+  <section id="hero">
+      <v-row align="center" justify="center">
+        <v-col cols="10">
+          <v-row align="center" justify="center">
+            <v-col cols="12" md="6" xl="8">
+              <h1 style="font-size: 60px; line-height: normal;" class="mb-4">Discover the<br/>solution to<br/>your home<br/>problems</h1>
+              <h4 class="font-weight-semi-bold">
+                Get trained professionals quickly and<br/>negotiate fair prices
+              </h4>
+            </v-col>
+            <v-col cols="12" md="6" xl="4">
+              <div >
+                <v-img src="@/assets/img/home2.svg" />
+              </div>
+            </v-col>
+          </v-row>
+        </v-col>
+      </v-row>
+  </section>
+</template>
+
+<script>
+  export default {
+
+  };
+</script>
+<style>
+  h1 {
+    color: #1A1A1B;
+  };
+  h4 {
+    color: #1A1A1B;
+    opacity: 0.5;
+  }
+
+</style>
